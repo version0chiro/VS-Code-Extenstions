1ad4ee0c7e53248fe723f2f6f70848a9dd125e41
Chiro <sachinsinghroxx@gmail.com>
2021-05-14 20:30:10 +0530

    adding postgres

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,20 @@
+import "reflect-metadata";
 import express from "express";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { join } from "path";
 
 const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    database: "vstodo",
+    username: "postgres",
+    password: "Sachin@123",
+    entities: [join(__dirname, "./entities/*.*")],
+    logging: !__prod__,
+    synchronize: !__prod__,
+  });
+
   const app = express();
 
   app.get("/", (_req, res) => {
